a0197d487ba0c0e8efddf9158d5f6f8624a107b6
Sravan_Kumar <devilsravankumar@gmail.com>
2025-02-24 21:12:50 +0530

    My first File

---
+++ demo.py
@@ -0,0 +1,9 @@
+user=int(input("Enter a number:"))
+if user>0:
+    print(user,"is a positive number")
+else:
+    if user==0:
+        print(user,"is a zero")
+    else:
+        print(user,"is a negative number")
+        
